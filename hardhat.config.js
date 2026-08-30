eb6e3fb2e62117e0363646dc787f9f4efa05c9f6
willi <willi@triology.com>
2026-08-29 21:35:08 -0500

    chore(deployments): live on-chain deployment & verification to Base Sepolia

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,4 +1,6 @@
-import 'dotenv/config';
+import dotenv from 'dotenv';
+dotenv.config();
+dotenv.config({ path: '.env.mainnet' });
 import hardhatEthers from '@nomicfoundation/hardhat-ethers';
 import hardhatVerify from '@nomicfoundation/hardhat-verify';
 import hardhatMocha from '@nomicfoundation/hardhat-mocha';
